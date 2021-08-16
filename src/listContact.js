54a0a8db1900122a3d2c51b31b4e7520ca9fd3f1
Dipo <adanikindipo@gmail.com>
2021-08-16 10:37:07 +0100

    add prop-types npm package and introduce controlled component

---
+++ src/listContact.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class ListContacts extends React.Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
+  };
+
+  render() {
+    return (
+      <div className="list-contacts">
+        <ol className="contact-list">
+          {this.props.contacts.map((contact) => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`,
+                }}
+              ></div>
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                onClick={() => this.props.onDeleteContact(contact)}
+                className="contact-remove"
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+// function ListContacts(props) {}
+
+// ListContacts.propTypes = {
+//   contacts: PropTypes.array.isRequired,
+//   onDeleteContact: PropTypes.func.isRequired,
+// };
+
+export default ListContacts;
