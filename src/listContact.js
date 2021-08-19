9343147de0300de23f5097683c48bb67a4942ccf
Dipo <adanikindipo@gmail.com>
2021-08-19 21:51:25 +0100

    Try to implement a very basic verrsion of the react routers using only state

--- src/listContact.js
+++ src/listContact.js
@@ -24,7 +24,7 @@ class ListContacts extends React.Component {
 
   render() {
     const { query } = this.state;
-    const { contacts, onDeleteContact } = this.props;
+    const { contacts, onDeleteContact, onNavigate } = this.props;
 
     const showingContacts =
       query === ""
@@ -43,6 +43,9 @@ class ListContacts extends React.Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
+          <a href="#create" onClick={onNavigate} className="add-contact">
+            Add Contact
+          </a>
         </div>
 
         {showingContacts.length !== contacts.length && (
